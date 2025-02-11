3f8d591c9fcc0c16d42d87ee02dc6aa8d27abab5
Pavlov Evgeniy <pavlov2001@bk.ru>
2025-02-11 16:10:25 +0300

    v1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,32 +1,35 @@
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
-  let postsService: PostsService;
+	let postsService: PostsService;
 
-  beforeEach(() => {
-    postsService = new PostsService();
-  });
+	beforeEach(() => {
+		postsService = new PostsService();
+	});
 
-  describe('.findMany', () => {
-    const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
-    ];
+	describe('.findMany', () => {
+		const posts = [
+			{ text: 'Post 1' },
+			{ text: 'Post 2' },
+			{ text: 'Post 3' },
+			{ text: 'Post 4' },
+		];
 
-    beforeEach(() => {
-      posts.forEach((post) => postsService.create(post));
-    });
+		beforeEach(() => {
+			posts.forEach((post) => postsService.create(post));
+		});
 
-    it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
-    });
+		it('should return all posts if called without options', () => {
+			const result = postsService.findMany();
+			expect(result).toEqual(posts);
+		});
 
-    it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
-    });
+		it('should return correct posts for skip and limit options', () => {
+			const result = postsService.findMany({ skip: 1, limit: 2 });
+			expect(result).toEqual([posts[1], posts[2]]);
+		});
 
-    // реализуйте недостающие тест-кейсы
-  });
+		
+
+	});
 });
